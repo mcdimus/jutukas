5b62b0bb53acac7008b351b38b85fafd73f88a0a
mcdimus@gmail.com <mcdimus@gmail.com@7bf3043d-fd97-a46d-590f-2021c541d702>
2011-12-01 20:47:10 +0000

    GUI

---
+++ trunk/Jutukas/src/client/ChatWindow.java
@@ -0,0 +1,214 @@
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+
+public class ChatWindow {
+
+	private JFrame frame;
+	private JTextField textField;
+	private final JEditorPane editorPane = new JEditorPane();
+
+	/**
+	 * Create the application.
+	 */
+	public ChatWindow() {
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public synchronized void appendText(String text) {
+		String editorPaneText = editorPane.getText();
+		editorPane.setText(editorPaneText.split("</body>")[0] + "<br>" + text + "</body>");
+	
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+//		KnownHostsManager khm = new KnownHostsManager();
+//		khm.getMapOfKnownHosts();
+//		khm.addNewHosts("[[\"Tanel Tammet\",\"22.33.44.55:6766\"],[\"Dmitri Laud\",\"22.33.44.11:6666\"]]");
+		frame = new JFrame();
+		frame.setTitle("Jutukas");
+		frame.setSize(800, 500);
+		UIutils.centerFrameOnScreen(frame);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		createMenuBar();
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+
+		JLabel lblNewLabel = new JLabel("Message:");
+		panel.add(lblNewLabel);
+
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(50);
+
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent arg0) {
+				appendText(textField.getText());
+				textField.setText("");
+			}
+		});
+		panel.add(btnNewButton);
+
+		Box horizontalBox = Box.createHorizontalBox();
+		panel.add(horizontalBox);
+
+		editorPane.setContentType("text/html");
+		editorPane.setText("<h1>Hello world</h1><br><i>blablalbalb</i>");
+		editorPane.setEditable(false);
+		
+		frame.getContentPane().add(editorPane);
+
+		
+	}
+
+	private void createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		createMenuFile(menuBar);
+
+		createMenuEdit(menuBar);
+
+		createMenuView(menuBar);
+
+		createMenuHelp(menuBar);
+	}
+
+	@SuppressWarnings("serial")
+	private void createMenuFile(JMenuBar menuBar) {
+		JMenu menuFile = new JMenu("File");
+		menuFile.setMnemonic('F');
+		menuBar.add(menuFile);
+
+		JMenuItem menuItemOpenLastSession = new JMenuItem(new AbstractAction("Open last session") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuItemOpenLastSession.setAccelerator(KeyStroke.getKeyStroke(
+				KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		menuFile.add(menuItemOpenLastSession);
+
+		JMenuItem menuItemSaveSession = new JMenuItem(new AbstractAction("Save session") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuItemSaveSession.setAccelerator(KeyStroke.getKeyStroke(
+				KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		menuFile.add(menuItemSaveSession);
+
+		JMenuItem menuItemQuit = new JMenuItem(new AbstractAction("Quit") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+			}
+		});
+		menuItemQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
+				InputEvent.CTRL_MASK));
+		menuFile.add(menuItemQuit);
+	}
+
+	@SuppressWarnings("serial")
+	private void createMenuEdit(JMenuBar menuBar) {
+		JMenu menuEdit = new JMenu("Edit");
+		menuEdit.setMnemonic('E');
+		menuBar.add(menuEdit);
+
+		JMenuItem menuItemFindUser = new JMenuItem(new AbstractAction("Find user...") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuItemFindUser.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
+				InputEvent.CTRL_MASK));
+		menuEdit.add(menuItemFindUser);
+
+		JMenuItem menuItemKnownUsersList = new JMenuItem(new AbstractAction("Known users list") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuItemKnownUsersList.setAccelerator(KeyStroke.getKeyStroke(
+				KeyEvent.VK_L, InputEvent.CTRL_MASK));
+		menuEdit.add(menuItemKnownUsersList);
+
+		JMenuItem menuItemUpdateUsersList = new JMenuItem(new AbstractAction("Update users list") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuItemUpdateUsersList.setAccelerator(KeyStroke.getKeyStroke(
+				KeyEvent.VK_U, InputEvent.CTRL_MASK));
+		menuEdit.add(menuItemUpdateUsersList);
+	}
+
+	@SuppressWarnings("serial")
+	private void createMenuView(JMenuBar menuBar) {
+		JMenu menuView = new JMenu("View");
+		menuView.setMnemonic('V');
+		menuBar.add(menuView);
+
+		JMenuItem menuItemClearChat = new JMenuItem(new AbstractAction("Clear chat") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuView.add(menuItemClearChat);
+
+	}
+
+	@SuppressWarnings("serial")
+	private void createMenuHelp(JMenuBar menuBar) {
+		JMenu menuHelp = new JMenu("Help");
+		menuBar.add(menuHelp);
+
+		JMenuItem menuItemAbout = new JMenuItem(new AbstractAction("About...") {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		menuHelp.add(menuItemAbout);
+
+	}
+}
--- trunk/Jutukas/src/client/MainWindow.java
+++ trunk/Jutukas/src/client/MainWindow.java
@@ -1,212 +1,437 @@
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
-import javax.swing.AbstractAction;
-import javax.swing.Box;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.KeyStroke;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.SoftBevelBorder;
 
 public class MainWindow {
 
 	private JFrame frame;
-	private JTextField textField;
-	private final JEditorPane editorPane = new JEditorPane();
+	private JLabel lblJutukas;
+
+	private JLabel lblStatus;
+	private JLabel lblIp;
+	private JLabel lblPort;
+	private JLabel lblName;
+	private JLabel lblStatusValue;
+	private JLabel lblIpValue;
+	private JLabel lblPortValue;
+	private JLabel lblNameValue;
+
+	private JButton btnAskNames;
+	private JButton btnFindName;
+	private JButton btnSendName;
+	private JButton btnConnect;
+	private JButton btnSettings;
+	private JButton btnClose;
+
+	private JLabel lblKnownUsers;
+	private JList knownUsersList;
+
+	private JPanel statusLinePanel;
+	private JLabel statusLine;
+
+	private JTextField nameToFind;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the application.
 	 */
 	public MainWindow() {
 		initialize();
-		frame.setVisible(true);
-	}
-	
-	public synchronized void appendText(String text) {
-		String editorPaneText = editorPane.getText();
-		editorPane.setText(editorPaneText.split("</body>")[0] + "<br>" + text + "</body>");
-	
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-//		KnownHostsManager khm = new KnownHostsManager();
-//		khm.getMapOfKnownHosts();
-//		khm.addNewHosts("[[\"Tanel Tammet\",\"22.33.44.55:6766\"],[\"Dmitri Laud\",\"22.33.44.11:6666\"]]");
-		frame = new JFrame();
-		frame.setTitle("Jutukas");
-		frame.setSize(800, 500);
-		UIutils.centerFrameOnScreen(frame);
+		frame = new JFrame("Jutukas");
+		frame.setSize(450, 300);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		UIutils.centerFrameOnScreen(frame);
 
-		createMenuBar();
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		lblJutukas = new JLabel("JUTUKAS");
+		lblJutukas.setFont(new Font("Dialog", Font.BOLD, 30));
 
-		JLabel lblNewLabel = new JLabel("Message:");
-		panel.add(lblNewLabel);
+		createPanelWithStatusLine();
+		createInformationBlock();
+		createButtons();
+		createKnownUsersList();
 
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(50);
-
-		JButton btnNewButton = new JButton("Send");
-		panel.add(btnNewButton);
-
-		Box horizontalBox = Box.createHorizontalBox();
-		panel.add(horizontalBox);
-
-		editorPane.setContentType("text/html");
-		editorPane.setText("<h1>Hello world</h1><br><i>blablalbalb</i>");
-		editorPane.setEditable(false);
-		
-		frame.getContentPane().add(editorPane);
-
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent arg0) {
-				appendText(textField.getText());
-			}
-		});
+		initializeLayout();
 	}
 
-	private void createMenuBar() {
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
+	/**
+	 * Initializes the group layout for the frame.
+	 */
+	private void initializeLayout() {
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 
-		createMenuFile(menuBar);
-
-		createMenuEdit(menuBar);
-
-		createMenuView(menuBar);
-
-		createMenuHelp(menuBar);
+		initializeHorizontalGroup(groupLayout);
+		initializeVerticalGroup(groupLayout);
 	}
 
-	@SuppressWarnings("serial")
-	private void createMenuFile(JMenuBar menuBar) {
-		JMenu menuFile = new JMenu("File");
-		menuFile.setMnemonic('F');
-		menuBar.add(menuFile);
-
-		JMenuItem menuItemOpenLastSession = new JMenuItem(new AbstractAction("Open last session") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuItemOpenLastSession.setAccelerator(KeyStroke.getKeyStroke(
-				KeyEvent.VK_O, InputEvent.CTRL_MASK));
-		menuFile.add(menuItemOpenLastSession);
-
-		JMenuItem menuItemSaveSession = new JMenuItem(new AbstractAction("Save session") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuItemSaveSession.setAccelerator(KeyStroke.getKeyStroke(
-				KeyEvent.VK_S, InputEvent.CTRL_MASK));
-		menuFile.add(menuItemSaveSession);
-
-		JMenuItem menuItemQuit = new JMenuItem(new AbstractAction("Quit") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				frame.dispose();
-			}
-		});
-		menuItemQuit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
-				InputEvent.CTRL_MASK));
-		menuFile.add(menuItemQuit);
+	/**
+	 * Sets horizontal group for the GroupLayout. This method is a true evil,
+	 * try not to read it.
+	 * 
+	 * @param groupLayout
+	 *            - layout.
+	 */
+	private void initializeHorizontalGroup(GroupLayout groupLayout) {
+		groupLayout
+				.setHorizontalGroup(groupLayout
+						.createParallelGroup(Alignment.TRAILING)
+						.addComponent(statusLinePanel,
+								GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+						.addGroup(
+								groupLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(lblJutukas)
+										.addPreferredGap(
+												ComponentPlacement.RELATED)
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.TRAILING)
+														.addComponent(
+																lblKnownUsers,
+																Alignment.LEADING)
+														.addComponent(
+																knownUsersList,
+																GroupLayout.DEFAULT_SIZE,
+																265,
+																Short.MAX_VALUE))
+										.addContainerGap())
+						.addGroup(
+								Alignment.LEADING,
+								groupLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.TRAILING,
+																false)
+														.addComponent(
+																btnClose,
+																Alignment.LEADING,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																Short.MAX_VALUE)
+														.addComponent(
+																btnSettings,
+																Alignment.LEADING,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																Short.MAX_VALUE)
+														.addGroup(
+																Alignment.LEADING,
+																groupLayout
+																		.createSequentialGroup()
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.LEADING)
+																						.addComponent(
+																								lblStatus)
+																						.addComponent(
+																								lblIp)
+																						.addComponent(
+																								lblPort)
+																						.addComponent(
+																								lblName))
+																		.addPreferredGap(
+																				ComponentPlacement.UNRELATED)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.LEADING)
+																						.addComponent(
+																								lblNameValue)
+																						.addComponent(
+																								lblPortValue)
+																						.addComponent(
+																								lblIpValue)
+																						.addComponent(
+																								lblStatusValue)))
+														.addComponent(
+																btnConnect,
+																Alignment.LEADING,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																Short.MAX_VALUE))
+										.addPreferredGap(
+												ComponentPlacement.RELATED)
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.TRAILING)
+														.addGroup(
+																Alignment.LEADING,
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				btnFindName)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				nameToFind,
+																				GroupLayout.DEFAULT_SIZE,
+																				152,
+																				Short.MAX_VALUE))
+														.addComponent(
+																btnAskNames,
+																Alignment.LEADING,
+																GroupLayout.DEFAULT_SIZE,
+																265,
+																Short.MAX_VALUE)
+														.addComponent(
+																btnSendName,
+																GroupLayout.DEFAULT_SIZE,
+																265,
+																Short.MAX_VALUE))
+										.addContainerGap()));
 	}
 
-	@SuppressWarnings("serial")
-	private void createMenuEdit(JMenuBar menuBar) {
-		JMenu menuEdit = new JMenu("Edit");
-		menuEdit.setMnemonic('E');
-		menuBar.add(menuEdit);
-
-		JMenuItem menuItemFindUser = new JMenuItem(new AbstractAction("Find user...") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuItemFindUser.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,
-				InputEvent.CTRL_MASK));
-		menuEdit.add(menuItemFindUser);
-
-		JMenuItem menuItemKnownUsersList = new JMenuItem(new AbstractAction("Known users list") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuItemKnownUsersList.setAccelerator(KeyStroke.getKeyStroke(
-				KeyEvent.VK_L, InputEvent.CTRL_MASK));
-		menuEdit.add(menuItemKnownUsersList);
-
-		JMenuItem menuItemUpdateUsersList = new JMenuItem(new AbstractAction("Update users list") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuItemUpdateUsersList.setAccelerator(KeyStroke.getKeyStroke(
-				KeyEvent.VK_U, InputEvent.CTRL_MASK));
-		menuEdit.add(menuItemUpdateUsersList);
+	/**
+	 * Sets vertical group for the GroupLayout. This method is a true evil, try
+	 * not to read it.
+	 * 
+	 * @param groupLayout
+	 *            - layout.
+	 */
+	private void initializeVerticalGroup(GroupLayout groupLayout) {
+		groupLayout
+				.setVerticalGroup(groupLayout
+						.createParallelGroup(Alignment.TRAILING)
+						.addGroup(
+								groupLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblJutukas)
+																		.addGap(5)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								lblStatus)
+																						.addComponent(
+																								lblStatusValue))
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								lblIp)
+																						.addComponent(
+																								lblIpValue))
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								lblPort)
+																						.addComponent(
+																								lblPortValue))
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								lblName)
+																						.addComponent(
+																								lblNameValue)))
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				lblKnownUsers)
+																		.addGap(7)
+																		.addComponent(
+																				knownUsersList,
+																				GroupLayout.PREFERRED_SIZE,
+																				100,
+																				GroupLayout.PREFERRED_SIZE)))
+										.addGap(18)
+										.addGroup(
+												groupLayout
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																Alignment.TRAILING,
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				btnConnect)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				btnSettings)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED,
+																				16,
+																				Short.MAX_VALUE)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.BASELINE)
+																						.addComponent(
+																								btnClose)
+																						.addComponent(
+																								btnSendName)))
+														.addGroup(
+																groupLayout
+																		.createSequentialGroup()
+																		.addComponent(
+																				btnAskNames)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addGroup(
+																				groupLayout
+																						.createParallelGroup(
+																								Alignment.LEADING,
+																								false)
+																						.addComponent(
+																								nameToFind)
+																						.addComponent(
+																								btnFindName))))
+										.addPreferredGap(
+												ComponentPlacement.RELATED)
+										.addComponent(statusLinePanel,
+												GroupLayout.PREFERRED_SIZE, 18,
+												GroupLayout.PREFERRED_SIZE)));
 	}
 
-	@SuppressWarnings("serial")
-	private void createMenuView(JMenuBar menuBar) {
-		JMenu menuView = new JMenu("View");
-		menuView.setMnemonic('V');
-		menuBar.add(menuView);
+	private void createKnownUsersList() {
+		lblKnownUsers = new JLabel("Known users:");
 
-		JMenuItem menuItemClearChat = new JMenuItem(new AbstractAction("Clear chat") {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuView.add(menuItemClearChat);
-
+		knownUsersList = new JList();
+		knownUsersList.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null,
+				null));
 	}
 
-	@SuppressWarnings("serial")
-	private void createMenuHelp(JMenuBar menuBar) {
-		JMenu menuHelp = new JMenu("Help");
-		menuBar.add(menuHelp);
+	private void createButtons() {
+		createStartButton();
+		createStopButton();
+		createSettingsButton();
+		createAskNamesButton();
+		createFindNameButtonWithTextField();
+		createSendNameButton();
+	}
 
-		JMenuItem menuItemAbout = new JMenuItem(new AbstractAction("About...") {
+	private void createStartButton() {
+		btnConnect = new JButton("Start");
+	}
 
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		menuHelp.add(menuItemAbout);
+	private void createStopButton() {
+		btnClose = new JButton("Stop");
+		btnClose.setEnabled(false);
+	}
 
+	private void createSettingsButton() {
+		btnSettings = new JButton("Settings");
+	}
+
+	private void createAskNamesButton() {
+		btnAskNames = new JButton("Ask names");
+		btnAskNames.setEnabled(false);
+	}
+
+	private void createFindNameButtonWithTextField() {
+
+		nameToFind = new JTextField();
+		nameToFind.setColumns(10);
+
+		btnFindName = new JButton("Find name");
+		btnFindName.setEnabled(false);
+	}
+
+	private void createSendNameButton() {
+		btnSendName = new JButton("Send name");
+		btnSendName.setEnabled(false);
+	}
+
+	/**
+	 * Creates information block with labels.
+	 * 
+	 * <pre>
+	 * Status: [running, not running]
+	 * IP:     [x.x.x.x]
+	 * Port:   [xxxx]
+	 * Name:   [kasutaja]
+	 * </pre>
+	 */
+	private void createInformationBlock() {
+		lblStatus = new JLabel("Status:");
+		lblIp = new JLabel("IP:");
+		lblPort = new JLabel("Port:");
+		lblName = new JLabel("Name:");
+		lblStatusValue = new JLabel("not running");
+		lblIpValue = new JLabel("127.0.0.1");
+		lblPortValue = new JLabel("6666");
+		lblNameValue = new JLabel("kasutaja");
+	}
+
+	/**
+	 * Creates panel with status line in the frame`s bottom.
+	 */
+	private void createPanelWithStatusLine() {
+		statusLinePanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) statusLinePanel.getLayout();
+		flowLayout.setVgap(0);
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		statusLinePanel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED,
+				null, null, null, null));
+
+		statusLine = new JLabel("Press connect to start the server.");
+		statusLine.setVerticalAlignment(SwingConstants.TOP);
+		statusLinePanel.add(statusLine);
 	}
 }
